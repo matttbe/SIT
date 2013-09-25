35a7b714fe1f3946d8335761120885632e743347
Matthieu Baerts <matttbe@gmail.com>
2013-09-25 21:28:40 +0200

    src: Added license and readme

---
+++ website/README.md
@@ -0,0 +1,26 @@
+Website for the Solidare-IT project
+===================================
+
+This website is coded in Ruby.
+
+Installation
+============
+
+**TODO**
+
+Authors
+=======
+
+Matthieu Baerts
+Benoît Baufays
+Julien Colmonts
+Benjamin Frantzen
+Pierre-Yves Légéna
+Vincent Van Ouytsel
+Ludovic Vannoorenberghe
+Alex Vermeylen
+
+License
+=======
+
+GPL3
---
+++ website/init.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+#
+## This file is part of Solidate-IT Website project.
+##
+## Copyright 2013 UCLouvain
+##  For the course: LSINF2255: Software Development Project
+##   http://uclouvain.be/en-cours-2013-LINGI2255.html
+##
+## Authors:
+##  Group 6 - Course: http://uclouvain.be/en-cours-2013-LINGI2255.html
+##  * Matthieu Baerts
+##  * Benoît Baufays
+##  * Julien Colmonts
+##  * Benjamin Frantzen
+##  * Pierre-Yves Légéna
+##  * Vincent Van Ouytsel
+##  * Ludovic Vannoorenberghe
+##  * Alex Vermeylen
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+##  MA 02110-1301, USA.
+
+print("Hello World!\n")
