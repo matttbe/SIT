f0d4e1e648bced5e292395002e1c0401c36cdcc0
Matthieu Baerts <matttbe@gmail.com>
2013-09-26 20:05:08 +0200

    Added Solidare-IT structure
    Files produced with 'rails new . --database=postgresql'

---
+++ website/Solidare-IT-g6/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolidareItG6::Application.config.secret_key_base = '80fc89249e34402f8975f94aed738f7f77ac645fd8e39d1b52d8e977c8d128339dc1235f733a24fba75b4853ebf8efe52dcba86316fb6a7ecdb5189d77927968'
