fe53f18c0d3cb05135288ba58e0c1d647be581cf
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-10-25 16:46:26 +0200

    ajout contraintes users

--- website/Solidare-IT-g6/app/models/user.rb
+++ website/Solidare-IT-g6/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :presence => true
+  validates :firstname, :presence => true
+  validates :birthdate, :presence => true,:date => { :before => Time.now }
+  validates :email, :presence => true,:format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :karma, :presence => true ,:numericality => :only_integer
+
 end
