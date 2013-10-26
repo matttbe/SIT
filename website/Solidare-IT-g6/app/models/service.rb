d3867bfba13ca19c70aa74c686bfe948699111ba
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-10-26 14:04:27 +0200

    add constraints

--- website/Solidare-IT-g6/app/models/service.rb
+++ website/Solidare-IT-g6/app/models/service.rb
@@ -1,2 +1,4 @@
 class Service < ActiveRecord::Base
+  validates :dateStart, :presence => true,:date => { :after => Time.now }
+  validates :dateEnd, :presence => true,:date => { :after => :start }
 end
