f0e6da42643b455cb5fcb0ccec43236900b373db
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-11-13 20:48:51 +0100

    remove services beacause we don't have the model

--- website/Solidare-IT-g6/db/schema.rb
+++ website/Solidare-IT-g6/db/schema.rb
@@ -46,14 +46,7 @@ ActiveRecord::Schema.define(version: 20131112090804) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-  create_table "services", force: true do |t|
-    t.text     "description"
-    t.date     "dateStart"
-    t.date     "dateEnd"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
+
 
   create_table "users", force: true do |t|
     t.string   "name"
