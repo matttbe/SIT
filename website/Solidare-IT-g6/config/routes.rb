f93fd0ac65c2bd027259b6a8795a58c0d18ae7c4
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-11-20 18:29:43 +0100

    fix issue #2

--- website/Solidare-IT-g6/config/routes.rb
+++ website/Solidare-IT-g6/config/routes.rb
@@ -6,7 +6,7 @@ SolidareItG6::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   #ActiveAdmin.routes(self)
-  resources :users
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
