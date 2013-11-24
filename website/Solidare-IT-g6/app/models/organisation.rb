eb5ee752ef05d00ff8293012cebf93a968cf3d97
alex <alex.vermeylen@gmail.com>
2013-11-24 12:02:07 +0100

    model for addresses + begining of its controller

--- website/Solidare-IT-g6/app/models/organisation.rb
+++ website/Solidare-IT-g6/app/models/organisation.rb
@@ -1,4 +1,5 @@
 class Organisation < ActiveRecord::Base
     validates :name, :presence => true
     has_many :own_services, :class_name => 'Service', :foreign_key => 'org_id'
+    has_many :addresses, :class_name => 'Address', :foreign_key => 'orga_id'
 end
