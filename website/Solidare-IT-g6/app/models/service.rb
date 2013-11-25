6e233f2279cff6cd61bc65e6a3eddf99f147c8b7
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-11-25 17:25:11 +0100

    change the service model for the transaction link

--- website/Solidare-IT-g6/app/models/service.rb
+++ website/Solidare-IT-g6/app/models/service.rb
@@ -8,7 +8,7 @@ class Service < ActiveRecord::Base
   belongs_to :user, :class_name => 'User', :foreign_key => 'creator_id'
   belongs_to :organisation, :class_name => 'Organisation', :foreign_key => 'org_id'
 
-  belongs_to :transaction, :class_name => 'Transaction', :foreign_key => 'service_id'
+  has_one :transaction, :class_name => 'Transaction', :foreign_key => 'service_id'
 
   has_one :matching_service, :class_name => 'Service', :foreign_key => 'matching_service_id'
   belongs_to :service
