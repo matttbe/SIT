14ce65dc0f982f5a04f4b803728a5e31bf72f869
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-11-28 20:20:58 +0100

    and now, service have one category

--- website/Solidare-IT-g6/app/models/category.rb
+++ website/Solidare-IT-g6/app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   resourcify
 
   belongs_to :parent, :class_name => 'Category', :foreign_key => 'parent'
+
+  has_many :services, :class_name => 'Service', :foreign_key => 'category_id'
 end
--- website/Solidare-IT-g6/db/schema.rb
+++ website/Solidare-IT-g6/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128122210) do
+ActiveRecord::Schema.define(version: 20131128191110) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20131128122210) do
     t.boolean  "is_demand",           default: false
     t.integer  "org_id"
     t.integer  "cat_id",              default: 1
+    t.integer  "category_id"
   end
 
   create_table "transactions", force: true do |t|
