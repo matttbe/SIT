9a875de3fcd2a1f9ac2ed3e8775cd07294afc29b
Matthieu Baerts <matttbe@gmail.com>
2013-11-28 13:16:14 +0100

    Organisation: resourcify

--- website/Solidare-IT-g6/app/models/organisation.rb
+++ website/Solidare-IT-g6/app/models/organisation.rb
@@ -1,8 +1,9 @@
 class Organisation < ActiveRecord::Base
-    validates :name, :presence => true
-    has_many :own_services, :class_name => 'Service', :foreign_key => 'org_id'
-    has_many :addresses, :class_name => 'Address', :foreign_key => 'orga_id'
-    has_many :coworkers
-    has_many :users, through: :coworkers
-    belongs_to :creator, :class_name=>'User', :foreign_key=>'creator_id'
+  resourcify
+  validates :name, :presence => true
+  has_many :own_services, :class_name => 'Service', :foreign_key => 'org_id'
+  has_many :addresses, :class_name => 'Address', :foreign_key => 'orga_id'
+  has_many :coworkers
+  has_many :users, through: :coworkers
+  belongs_to :creator, :class_name=>'User', :foreign_key=>'creator_id'
 end
