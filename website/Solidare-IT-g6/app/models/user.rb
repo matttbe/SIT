70d97e9493cae19b362a7a80bc6789a274e010da
Benj <benj@Bbj-Ubuntu.(none)>
2013-11-29 11:53:10 +0100

    comments in groups

--- website/Solidare-IT-g6/app/models/user.rb
+++ website/Solidare-IT-g6/app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
 
   has_many :own_services, :class_name => 'Service', :foreign_key => 'creator_id'
   has_many :group_posts, :class_name => 'GroupPost', :foreign_key => 'user_id'
+  has_many :group_post_comments, :class_name => 'GroupPostComment', :foreign_key => 'user_id'
   def all_name
     "#{firstname} #{name}"
   end
