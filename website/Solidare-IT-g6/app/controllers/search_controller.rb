8a23b9548b70b7efacca1cb1cfeff11a0b63d3bf
Matthieu Baerts <matttbe@gmail.com>
2013-11-30 19:20:29 +0100

    Ctrl: Search: fixed Active Record warnings
    Fixed this warning: 'DEPRECATION WARNING: It looks like you are eager loading table(s) (one of: services, categories) that are referenced in a string SQL snippet'

--- website/Solidare-IT-g6/app/controllers/search_controller.rb
+++ website/Solidare-IT-g6/app/controllers/search_controller.rb
@@ -27,7 +27,7 @@ class SearchController < ApplicationController
       end 
 
       #includes category infos
-      @services=@services.includes(:category)
+      @services=@services.includes(:category).references(:category)
       if (! params[:q].nil? and ! params[:q].empty? and !@services.nil?)
           @services = @services.where(:quick_match=>false).where('(services.title LIKE (:titles) or services.description LIKE (:titles) or categories.title LIKE (:titles) or categories.text LIKE (:titles))',
                    :titles => "%" + params[:q] + "%")
