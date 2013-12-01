1a29ce04c2e0dabece4edd54943efd9b1e4c5ddc
Benj <benj@Bbj-Ubuntu.(none)>
2013-12-01 15:20:15 +0100

    user not added if already in group

--- website/Solidare-IT-g6/app/controllers/group_user_relation_controller.rb
+++ website/Solidare-IT-g6/app/controllers/group_user_relation_controller.rb
@@ -1,14 +1,14 @@
 class GroupUserRelationController < ApplicationController
   def new
-  	@relation = GroupUserRelation.new(user_id:params[:idUser] , group_id:params[:idGroup])
-  	#TODO check if the relation already exists
-  	respond_to do |format|
-  		if @relation.save
-			format.json { render json: @relation}
-		else
-			format.json { render json: {:error => "error"}} #TODO 
+	unless Relations.where(:id => params[:idUser]).blank ?
+  		@relation = GroupUserRelation.new(user_id:params[:idUser] , group_id:params[:idGroup])
+  		respond_to do |format|
+  			if @relation.save
+				format.json { render json: @relation}
+			else
+				format.json { render json: {:error => "error"}} #TODO 
+			end
 		end
     end
-       
   end
 end
