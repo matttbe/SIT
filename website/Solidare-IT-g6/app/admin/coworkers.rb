fc8cf7f029ec7a33e02a84fa5b0d08788d704a95
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-12-03 12:07:29 +0100

    add a delete link for coworker

--- website/Solidare-IT-g6/app/admin/coworkers.rb
+++ website/Solidare-IT-g6/app/admin/coworkers.rb
@@ -1,5 +1,17 @@
-ActiveAdmin.register Coworker, :as => "Coworker", namespace: :organisation_manage  do
+ActiveAdmin.register Coworker, :as => "Coworkers", namespace: :organisation_manage  do
   config.clear_action_items!
+
+  member_action :remove_coworker, :method => :get, :param =>[:id, :id_coworker] do
+    coworker = Coworker.find(params[:id])
+    
+    if !coworker.nil?
+      coworker.destroy
+    #Notifier.admin_validated(user).deliver
+    end
+    respond_to do |format|
+      format.html{ redirect_to request.referer}
+    end
+  end
 
   index do
     
