608c7dafaa84d793ba3931d22d8fbb60664375d2
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-12-03 15:53:12 +0100

    add the new corworker method

--- website/Solidare-IT-g6/app/admin/coworkers.rb
+++ website/Solidare-IT-g6/app/admin/coworkers.rb
@@ -1,7 +1,25 @@
 ActiveAdmin.register Coworker, :as => "Coworkers", namespace: :organisation_manage  do
   config.clear_action_items!
+  #form :partial =>"form"
 
-  member_action :remove_coworker, :method => :get, :param =>[:id, :id_coworker] do
+  form do |f|
+    f.inputs "User" do
+      f.collection_select :user_id, User.all, :id, :all_name
+    end
+    f.inputs "Organisation" do
+      f.collection_select :organisation_id, Organisation.where("creator_id=:id",:id=>current_user.id), :id, :name
+    end
+    f.actions
+  end
+
+
+  controller do
+    def permitted_params
+      params.permit(:coworkers => [:organisation_id, :user_id])
+    end
+  end
+
+  member_action :remove_coworker, :method => :get do
     coworker = Coworker.find(params[:id])
     
     if !coworker.nil?
@@ -16,6 +34,7 @@ ActiveAdmin.register Coworker, :as => "Coworkers", namespace: :organisation_mana
   index do
     
   end
+
 
   member_action :organisation do
     @organisation = Organisation.find(params[:id])
