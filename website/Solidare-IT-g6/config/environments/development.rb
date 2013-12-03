0d6808a1703ddfc422f897df932a649a3bcf02c4
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-12-03 08:58:53 +0100

    add mailer support

--- website/Solidare-IT-g6/config/environments/development.rb
+++ website/Solidare-IT-g6/config/environments/development.rb
@@ -26,4 +26,19 @@ SolidareItG6::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    :user_name => "solidare.it.6@gmail.com",
+    #TODO protect this
+    :password => 'iloveponcin',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
