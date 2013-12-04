8fa7ef88215628271367874fb428e30f4f1b0f40
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-12-04 14:05:17 +0100

    and now : is there

--- website/Solidare-IT-g6/db/migrate/20131204122627_add_coworker_id_to_users.rb
+++ website/Solidare-IT-g6/db/migrate/20131204122627_add_coworker_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddCoworkerIdToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :coworker_id, :integer, default =>-1
+    add_column :users, :coworker_id, :integer, :default =>-1
   end
 end
