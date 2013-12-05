1b4ebedd6d60f9eed0391409ec26969392af1097
ben.fra <benj@Bbj-Ubuntu.(none)>
2013-12-05 22:36:50 +0100

    delete group if secret

--- website/Solidare-IT-g6/app/controllers/group_controller.rb
+++ website/Solidare-IT-g6/app/controllers/group_controller.rb
@@ -73,10 +73,17 @@ class GroupController < ApplicationController
 	  GroupUserRelation.destroy(t_d.id)
 	end
 	@user = User.find(params[:u_id])
-	if @user == current_user
+	
+	@group = Group.find(params[:g_id])
+	if @group.secret && GroupUserRelation.where(group_id: params[:g_id]).blank?#no more user on group	
+		@group.destroy
 		redirect_to group_index_path
 	else
-		redirect_to edit_group_path(Group.find(params[:g_id]))
+		if @user == current_user
+			redirect_to group_index_path
+		else
+			redirect_to edit_group_path(Group.find(params[:g_id]))
+		end
 	end
   end
 
