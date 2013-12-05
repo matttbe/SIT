84a9f9457e6178b576d9f1e39118de717e31bdcd
Matthieu Baerts <matttbe@gmail.com>
2013-12-05 15:45:11 +0100

    Notifications Ctrl: Fixed PG error

--- website/Solidare-IT-g6/app/controllers/notifications_controller.rb
+++ website/Solidare-IT-g6/app/controllers/notifications_controller.rb
@@ -2,11 +2,11 @@ class NotificationsController < ApplicationController
   
   def index
     if user_signed_in?
-      @notifications = Notification.where("notified_user == :user_id", :user_id => current_user.id)
-  	  @notifications.each do |notif|
-  	    if notif.seen == false
-    		  notif.seen = true
-    		  notif.save
+      @notifications = Notification.where(:notified_user => current_user.id)
+      @notifications.each do |notif|
+        if notif.seen == false
+          notif.seen = true
+          notif.save
         end
       end
       @users = User.all
