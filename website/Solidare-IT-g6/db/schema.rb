65b129e0e5c1b72f8b58c97e36d524faf0125f02
Ludovic Vannoorenberghe <ludovic.v@me.com>
2013-12-06 00:19:10 +0100

    button gris

--- website/Solidare-IT-g6/db/schema.rb
+++ website/Solidare-IT-g6/db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131205225557) do
 
   create_table "accept_services", force: true do |t|
