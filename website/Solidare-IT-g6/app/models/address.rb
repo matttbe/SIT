14408d8456ec1baab009b9c1cb057837a1acdf5f
alex <alex.vermeylen@gmail.com>
2013-12-05 14:00:29 +0100

    badges + address model + back button style

--- website/Solidare-IT-g6/app/models/address.rb
+++ website/Solidare-IT-g6/app/models/address.rb
@@ -5,14 +5,23 @@ class Address < ActiveRecord::Base
   validates :postal_code, :presence => true 
   validates :city, :presence => true
   validates :country, :presence => true
+  validates :latitude, :presence => true
+  validates :longitude, :presence => true
 
   has_many :services, :class_name => 'Service', :foreign_key => 'address_id'
   
-  #TODO : How to make sure that (at least) one of the two fields is valid and present ?
+  validates :user_id, :presence => true, :if => :orga_present?
+  validates :orga_id, :presence => true, :if => :user_present?
+
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
   belongs_to :organisation, :class_name => 'Organisation', :foreign_key => 'orga_id'
 
-
+  def orga_present?
+    self.orga_id.nil?
+  end
   
+  def user_present?
+    self.user_id.nil?
+  end
 
 end
