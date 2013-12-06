ed8b92fbf80a1ac269d2369f1f5f7f73fc3b14d1
Benoit Baufays <benoit.baufays@conceptbandb.be>
2013-12-06 23:39:51 +0100

    category ok

--- website/Solidare-IT-g6/app/admin/categories.rb.rb
+++ website/Solidare-IT-g6/app/admin/categories.rb.rb
@@ -1,2 +1,8 @@
 ActiveAdmin.register Category, :as => "Category" do
+
+  controller do
+    def permitted_params
+      params.permit(:category => [:parent,:title,:text, :date_start, :date_end,:is_demand, :photo, :creator_id])
+    end
+  end
 end
