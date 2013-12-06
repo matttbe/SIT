5e989dca480c0d577f45ad6936e0660bef0fbb44
Matthieu Baerts <matttbe@gmail.com>
2013-12-06 03:44:46 +0100

    Fixed cucumber [ci skip]

--- website/Solidare-IT-g6/app/controllers/application_controller.rb
+++ website/Solidare-IT-g6/app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
     if (request.fullpath != "/users/sign_in" &&
         request.fullpath != "/users/sign_up" &&
         request.fullpath != "/users" &&
+        request.fullpath != "/create_account" &&
         request.fullpath != "/users/password" &&
         !request.xhr?) # don't store ajax calls
       session[:previous_url] = request.fullpath
