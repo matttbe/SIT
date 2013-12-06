9fc120e6f2b63e1123e813fd827332acfcda07a0
Matthieu Baerts <matttbe@gmail.com>
2013-12-06 21:41:00 +0100

    Avoid warning I18n

--- website/Solidare-IT-g6/config/application.rb
+++ website/Solidare-IT-g6/config/application.rb
@@ -19,5 +19,10 @@ module SolidareItG6
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # [deprecated] I18n.enforce_available_locales will default to true in the
+    # future. If you really want to skip validation of your locale you can set
+    # I18n.enforce_available_locales = false to avoid this message
+    I18n.enforce_available_locales = false
   end
 end
